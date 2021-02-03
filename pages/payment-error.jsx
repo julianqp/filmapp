6fd5ced73987d2fa2ad733413772acbd772bf432
Julian Querol <julian.querol.p@gmail.com>
2021-02-04 00:02:23 +0100

    error page

---
+++ pages/payment-error.jsx
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout'
+
+const PaymentError = () => (
+  <Layout>
+    <p className="text-center">Error en el pago</p>
+  </Layout>
+)
+export default PaymentError
